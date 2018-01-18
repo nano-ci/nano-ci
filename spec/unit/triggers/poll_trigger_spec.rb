1e16cb9c8c299d92e2e13dab8236159d044a7f14
Andrew Maraev <the_vk@thevk.net>
2018-01-17 20:00:00 -0800

    unit tests for PollTrigger related to EventMachine

--- spec/unit/triggers/poll_trigger_spec.rb
+++ spec/unit/triggers/poll_trigger_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Nanoci::Triggers::PollTrigger do
     expect(event_machine).to receive(:add_periodic_timer).with(5)
     trigger.run
   end
+
+  it 'calls project.trigger_build is repo.detect_changes returns true' do
+    repo = double('repo')
+    allow(repo).to receive(:detect_changes).and_return(true)
+    project = double('repo')
+
+    trigger = Nanoci::Triggers::PollTrigger.new(repo, project, 'interval' => 5)
+    event_machine = class_double(EventMachine).as_stubbed_const
+    allow(event_machine).to receive(:add_periodic_timer).and_yield
+
+    expect(project).to receive(:trigger_build).with(trigger)
+
+    trigger.run
+  end
+
+  it 'does not call project.trigger_build is repo.detect_changes returns false' do
+    repo = double('repo')
+    allow(repo).to receive(:detect_changes).and_return(false)
+    project = double('repo')
+
+    trigger = Nanoci::Triggers::PollTrigger.new(repo, project, 'interval' => 5)
+    event_machine = class_double(EventMachine).as_stubbed_const
+    allow(event_machine).to receive(:add_periodic_timer).and_yield
+
+    expect(project).not_to receive(:trigger_build)
+
+    trigger.run
+  end
 end
