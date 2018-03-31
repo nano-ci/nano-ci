8645d9dbbdf7853150df83a25329d42e276a6aa4
Andrew Maraev <the_vk@thevk.net>
2018-03-30 20:00:00 -0800

    updated state_manager to use colletion per type

--- lib/nanoci/state_manager.rb
+++ lib/nanoci/state_manager.rb
@@ -21,15 +21,14 @@ class Nanoci
 
     def put_state(type, state)
       data = state.clone
-      data[TYPE] = type
       data[CREATED_UTC] = Time.now
-      collection = @client[:state]
+      collection = @client[type]
       collection.insert_one(data)
     end
 
     def get_state(type, tag)
-      collection = @client[:state]
-      collection.find(type: type, tag: tag).sort(CREATED_UTC.to_sym => -1).first
+      collection = @client[type]
+      collection.find(tag: tag).sort(CREATED_UTC.to_sym => -1).first
     end
   end
 end
