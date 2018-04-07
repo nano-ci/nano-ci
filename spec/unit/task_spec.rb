4c22c2bd58cf4eae98612c8b38b3e686a54fd035
Andrew Maraev <the_vk@thevk.net>
2018-04-06 20:00:00 -0800

    fixed Task unit tests

--- spec/unit/task_spec.rb
+++ spec/unit/task_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe Nanoci::Task do
     expect(Nanoci::Task.types).to be_a Hash
   end
 
-  it 'saves type from src' do
-    task = Nanoci::Task.new('type' => 'make')
+  it 'reads type from src' do
+    task = Nanoci::Task.new(type: 'make')
     expect(task.type).to eq 'make'
   end
 
   it 'required_agent_capabilities returns an empty Set' do
-    task = Nanoci::Task.new('type' => 'make')
+    task = Nanoci::Task.new(type: 'make')
     expect(task.required_agent_capabilities(nil)).to be_an Set
     expect(task.required_agent_capabilities(nil).length).to eq 0
   end
