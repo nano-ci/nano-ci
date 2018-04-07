5a82b4249704ec932d68b4a9d25f3575e6c20f27
Andrew Maraev <the_vk@thevk.net>
2018-04-06 20:00:00 -0800

    fixed Repo unit tests

--- spec/unit/repo_spec.rb
+++ spec/unit/repo_spec.rb
@@ -3,33 +3,33 @@ require 'spec_helper'
 require 'nanoci/repo'
 
 RSpec.describe Nanoci::Repo do
-  it 'saves tag from src' do
-    repo = Nanoci::Repo.new('tag' => 'repo-tag')
+  it 'reads tag from src' do
+    repo = Nanoci::Repo.new(tag: 'repo-tag')
     expect(repo.tag).to eq 'repo-tag'
   end
 
-  it 'saves type from src' do
-    repo = Nanoci::Repo.new('type' => 'repo-tag')
+  it 'reads type from src' do
+    repo = Nanoci::Repo.new(type: 'repo-tag')
     expect(repo.type).to eq 'repo-tag'
   end
 
-  it 'set type to "unknown" it type is not set in src' do
+  it 'type default value is "unknown"' do
     repo = Nanoci::Repo.new
     expect(repo.type).to eq 'unknown'
   end
 
-  it 'saves src from src' do
-    repo = Nanoci::Repo.new('src' => 'http://example.com/repo/test.git')
+  it 'reads src from src' do
+    repo = Nanoci::Repo.new(src: 'http://example.com/repo/test.git')
     expect(repo.src).to eq 'http://example.com/repo/test.git'
   end
 
-  it 'saves auth from src' do
-    repo = Nanoci::Repo.new('auth' => 'username:password')
+  it 'reads auth from src' do
+    repo = Nanoci::Repo.new(auth: 'username:password')
     expect(repo.auth).to eq 'username:password'
   end
 
   it 'reads main from src' do
-    repo = Nanoci::Repo.new('main' => true)
+    repo = Nanoci::Repo.new(main: true)
     expect(repo.main).to be true
   end
 
@@ -49,24 +49,18 @@ RSpec.describe Nanoci::Repo do
   end
 
   it 'state returns tag' do
-    repo = Nanoci::Repo.new('tag' => 'repo-tag')
+    repo = Nanoci::Repo.new(tag: 'repo-tag')
     expect(repo.state[:tag]).to eq 'repo-tag'
   end
 
   it 'state returns current_commit' do
-    class TestRepo < Nanoci::Repo
-      def initialize(*args)
-        super
-        @current_commit = 'abc'
-      end
-    end
-
-    repo = TestRepo.new('tag' => 'repo-tag')
+    repo = Nanoci::Repo.new(tag: 'repo-tag')
+    repo.current_commit = 'abc'
     expect(repo.state[:current_commit]).to eq 'abc'
   end
 
   it 'state restures current_commit' do
-    repo = Nanoci::Repo.new('tag' => 'repo-tag')
+    repo = Nanoci::Repo.new(tag: 'repo-tag')
     state = { tag: 'repo-tag', current_commit: 'abc' }
     repo.state = state
     expect(repo.current_commit).to eq 'abc'
