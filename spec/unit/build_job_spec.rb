14b30fbf7f842fe20417ecf5a8ef04bec06ebcd9
Andrew Maraev <the_vk@thevk.net>
2018-08-21 09:46:26 -0700

    fixed BuildJob unit tests

--- spec/unit/build_job_spec.rb
+++ spec/unit/build_job_spec.rb
@@ -4,22 +4,34 @@ require 'nanoci/build_job'
 require 'nanoci/job'
 require 'nanoci/task'
 
-class TestTask < Nanoci::Task
-  def required_agent_capabilities(_)
-    Set['test.cap']
-  end
-end
-
 RSpec.describe Nanoci::BuildJob do
   it 'saves passed job to field' do
-    job = Nanoci::Job.new('tag' => 'test')
+    job_def = Nanoci::Definition::JobDefinition.new(
+      tag: 'test'
+    )
+    job = Nanoci::Job.new(job_def, nil)
     build_job = Nanoci::BuildJob.new(job)
     expect(build_job.definition).to eq(job)
   end
 
   it 'returns required agent capabilities from job definition' do
-    job = Nanoci::Job.new('tag' => 'test')
-    job.tasks = [TestTask.new]
+    Nanoci.resources.clean
+
+    class TestTask < Nanoci::Task
+      provides 'test-task'
+      def required_agent_capabilities
+        Set['test.cap']
+      end
+    end
+
+    job_def = Nanoci::Definition::JobDefinition.new(
+      tag: 'test',
+      tasks: [
+        type: 'test-task'
+      ]
+    )
+
+    job = Nanoci::Job.new(job_def, nil)
     build_job = Nanoci::BuildJob.new(job)
     expect(build_job.required_agent_capabilities).to eq(job.required_agent_capabilities)
   end
