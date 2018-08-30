d1359109644ad8dd9d21fbf078f6c98cf219d99e
Andrew Maraev <the_vk@thevk.net>
2018-08-29 21:52:41 -0700

    fixed BuildStage unit tests

--- spec/unit/build_stage_spec.rb
+++ spec/unit/build_stage_spec.rb
@@ -1,40 +1,48 @@
 require 'spec_helper'
 
 require 'nanoci/build_stage'
+require 'nanoci/definition/stage_definition'
 require 'nanoci/job'
 require 'nanoci/project'
 require 'nanoci/stage'
 
 RSpec.describe Nanoci::BuildStage do
   it 'creates build job for for each job definition' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    job = Nanoci::Job.new(project, 'tag' => 'test-job')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [job]
-
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [{
+        tag: 'test-job'
+      }]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
     build_stage = Nanoci::BuildStage.new(stage)
     expect(build_stage.jobs).not_to be_nil
     expect(build_stage.jobs.length).to eq(1)
-    expect(build_stage.jobs[0].definition).to eq(job)
+    expect(build_stage.jobs[0].definition).to eq(stage.jobs[0])
   end
 
   it 'creates build stage for for stage definition' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    job = Nanoci::Job.new(project, 'tag' => 'test-job')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [job]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [{
+        tag: 'test-job'
+      }]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     expect(build_stage.definition).to eq(stage)
   end
 
   it 'to be QUEUED if at least one job is QUEUED and no UNDEFINED' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [
-      Nanoci::Job.new(project, 'tag' => 'test-job'),
-      Nanoci::Job.new(project, 'tag' => 'test-job-1')
-    ]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [
+        { tag: 'test-job' },
+        { tag: 'test-job-1' }
+      ]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     build_stage.jobs[0].state = Nanoci::Build::State::QUEUED
@@ -43,12 +51,14 @@ RSpec.describe Nanoci::BuildStage do
   end
 
   it 'to be RUNNING if at least one job is RUNNING and no UNDEFINED or QUEUED' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [
-      Nanoci::Job.new(project, 'tag' => 'test-job'),
-      Nanoci::Job.new(project, 'tag' => 'test-job-1')
-    ]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [
+        { tag: 'test-job' },
+        { tag: 'test-job-1' }
+      ]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     build_stage.jobs[0].state = Nanoci::Build::State::RUNNING
@@ -57,12 +67,14 @@ RSpec.describe Nanoci::BuildStage do
   end
 
   it 'to be ABORTED if at least one job is ABORTED and no UNDEFINED, QUEUED or RUNNING' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [
-      Nanoci::Job.new(project, 'tag' => 'test-job'),
-      Nanoci::Job.new(project, 'tag' => 'test-job-1')
-    ]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [
+        { tag: 'test-job' },
+        { tag: 'test-job-1' }
+      ]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     build_stage.jobs[0].state = Nanoci::Build::State::ABORTED
@@ -71,12 +83,14 @@ RSpec.describe Nanoci::BuildStage do
   end
 
   it 'to be FAILED if at least one job is FAILED and no UNDEFINED, QUEUED, RUNNING or ABORTED' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [
-      Nanoci::Job.new(project, 'tag' => 'test-job'),
-      Nanoci::Job.new(project, 'tag' => 'test-job-1')
-    ]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [
+        { tag: 'test-job' },
+        { tag: 'test-job-1' }
+      ]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     build_stage.jobs[0].state = Nanoci::Build::State::FAILED
@@ -85,12 +99,14 @@ RSpec.describe Nanoci::BuildStage do
   end
 
   it 'to be COMPLETED if all jobs are COMPLETED' do
-    project = Nanoci::Project.new('tag' => 'test', 'name' => 'test project')
-    stage = Nanoci::Stage.new('tag' => 'test')
-    stage.jobs = [
-      Nanoci::Job.new(project, 'tag' => 'test-job'),
-      Nanoci::Job.new(project, 'tag' => 'test-job-1')
-    ]
+    stage_def = Nanoci::Definition::StageDefinition.new(
+      tag: 'test',
+      jobs: [
+        { tag: 'test-job' },
+        { tag: 'test-job-1' }
+      ]
+    )
+    stage = Nanoci::Stage.new(stage_def, nil)
 
     build_stage = Nanoci::BuildStage.new(stage)
     build_stage.jobs[0].state = Nanoci::Build::State::COMPLETED
