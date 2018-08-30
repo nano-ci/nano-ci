5fe8a3d68084ff2ecf9011f2441fc4c4d2725f54
Andrew Maraev <the_vk@thevk.net>
2018-08-29 21:42:07 -0700

    fixed Build unit tests

--- spec/unit/build_spec.rb
+++ spec/unit/build_spec.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 require 'nanoci/build'
+require 'nanoci/definition/project_definition'
+require 'nanoci/definition/variable_definition'
 require 'nanoci/project'
 require 'nanoci/repo'
 require 'nanoci/trigger'
@@ -8,123 +12,193 @@ require 'nanoci/variable'
 
 RSpec.describe Nanoci::Build do
   it 'saves reference to project' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
     expect(build.project).to eq project
   end
 
   it 'saves reference to trigger' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }]
+    )
+    project = Nanoci::Project.new(project_def)
     trigger = Nanoci::Trigger.new(nil)
     build = Nanoci::Build.run(project, trigger, {}, 'build_data_dir' => '/abc')
     expect(build.trigger).to eq trigger
   end
 
   it 'merge project variables' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
-    project.variables = {
-      'var1' => Nanoci::Variable.new(tag: 'var1', value: 'var1 value'),
-      'var2' => Nanoci::Variable.new(tag: 'var2', value: 'var2 value')
-    }
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
-    expect(build.variables).to include 'var1'
-    expect(build.variables['var1']).to eq 'var1 value'
-    expect(build.variables).to include 'var2'
-    expect(build.variables['var2']).to eq 'var2 value'
+    expect(build.variables).to include :var1
+    expect(build.variables[:var1]).to eq 'var1 value'
+    expect(build.variables).to include :var2
+    expect(build.variables[:var2]).to eq 'var2 value'
   end
 
   it 'merge env variables' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }]
+    )
+    project = Nanoci::Project.new(project_def)
     env_vars = {
-      'var1' => Nanoci::Variable.new(tag: 'var1', value: 'var1 value'),
-      'var2' => Nanoci::Variable.new(tag: 'var2', value: 'var2 value')
+      :var1 => Nanoci::Variable.new(Nanoci::Definition::VariableDefinition.new(tag: 'var1', value: 'var1 value')),
+      :var2 => Nanoci::Variable.new(Nanoci::Definition::VariableDefinition.new(tag: 'var2', value: 'var2 value'))
     }
     trigger = Nanoci::Trigger.new(nil)
     build = Nanoci::Build.run(project, trigger, env_vars, 'build_data_dir' => '/abc')
-    expect(build.variables).to include 'var1'
-    expect(build.variables['var1']).to eq 'var1 value'
-    expect(build.variables).to include 'var2'
-    expect(build.variables['var2']).to eq 'var2 value'
+    expect(build.variables).to include :var1
+    expect(build.variables[:var1]).to eq 'var1 value'
+    expect(build.variables).to include :var2
+    expect(build.variables[:var2]).to eq 'var2 value'
   end
 
   it 'merge project and env variables' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
-    project.variables = {
-      'var1' => Nanoci::Variable.new(tag: 'var1', value: 'var1 value'),
-      'var2' => Nanoci::Variable.new(tag: 'var2', value: 'var2 value')
-    }
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     env_vars = {
-      'var3' => Nanoci::Variable.new(tag: 'var3', value: 'var3 value'),
-      'var4' => Nanoci::Variable.new(tag: 'var4', value: 'var4 value')
+      :var3 => Nanoci::Variable.new(Nanoci::Definition::VariableDefinition.new(tag: 'var3', value: 'var3 value')),
+      :var4 => Nanoci::Variable.new(Nanoci::Definition::VariableDefinition.new(tag: 'var4', value: 'var4 value'))
     }
     trigger = Nanoci::Trigger.new(nil)
     build = Nanoci::Build.run(project, trigger, env_vars, 'build_data_dir' => '/abc')
-    expect(build.variables).to include 'var1'
-    expect(build.variables['var1']).to eq 'var1 value'
-    expect(build.variables).to include 'var2'
-    expect(build.variables['var2']).to eq 'var2 value'
-    expect(build.variables).to include 'var3'
-    expect(build.variables['var3']).to eq 'var3 value'
-    expect(build.variables).to include 'var4'
-    expect(build.variables['var4']).to eq 'var4 value'
+    expect(build.variables).to include :var1
+    expect(build.variables[:var1]).to eq 'var1 value'
+    expect(build.variables).to include :var2
+    expect(build.variables[:var2]).to eq 'var2 value'
+    expect(build.variables).to include :var3
+    expect(build.variables[:var3]).to eq 'var3 value'
+    expect(build.variables).to include :var4
+    expect(build.variables[:var4]).to eq 'var4 value'
   end
 
   it 'sets start time about now' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
     expect(build.start_time).to be_within(1).of(Time.now)
   end
 
   it 'sets tag using project tag and number' do
-    project = Nanoci::Project.new(tag: 'tag-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'tag-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
     expect(build.tag).to eq('tag-test-1')
   end
 
   it 'sets sets current stage to project first stage' do
-    project = Nanoci::Project.new(tag: 'tag-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
-    expect(build.current_stage.definition).to eq(stage)
+    expect(build.current_stage.definition).to eq(project.stages[0])
   end
 
   it 'sets build commits equal to project repo current commits' do
-    project = Nanoci::Project.new(tag: 'tag-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     repo = double('repo')
     allow(repo).to receive(:tag).and_return('repo-1')
     allow(repo).to receive(:current_commit).and_return('abcdef')
@@ -132,30 +206,50 @@ RSpec.describe Nanoci::Build do
     allow(repo).to receive(:in_repo_cache).and_yield
     allow(repo).to receive(:update)
     allow(repo).to receive(:branch)
-    allow(repo).to receive(:tip_of_tree )
-    project.repos = { 'repo-1' => repo }
+    allow(repo).to receive(:tip_of_tree)
+    project.repos['repo-1'] = repo
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
     expect(build.commits).to include 'repo-1'
     expect(build.commits['repo-1']).to eq 'abcdef'
   end
 
   it 'initial state is QUEUED' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
 
     expect(build.state).to eq Nanoci::Build::State::QUEUED
   end
 
   it 'state is equal to current stage state' do
-    project = Nanoci::Project.new(tag: 'project-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
 
     expect(build.current_stage.state).to eq Nanoci::Build::State::QUEUED
@@ -168,11 +262,21 @@ RSpec.describe Nanoci::Build do
   end
 
   it 'workdir equals to agent workdir + build tag' do
-    project = Nanoci::Project.new(tag: 'project-path-test', name: 'test project')
-    job = Nanoci::Job.new(project, tag: 'test-job')
-    stage = Nanoci::Stage.new(tag: 'test')
-    stage.jobs = [job]
-    project.stages = [stage]
+    project_def = Nanoci::Definition::ProjectDefinition.new(
+      tag: 'project-path-test',
+      name: 'test project',
+      stages: [{
+        tag: 'test',
+        jobs: [{
+          tag: 'test-job'
+        }]
+      }],
+      variables: [
+        { tag: 'var1', value: 'var1 value' },
+        { tag: 'var2', value: 'var2 value' }
+      ]
+    )
+    project = Nanoci::Project.new(project_def)
     build = Nanoci::Build.run(project, nil, {}, 'build_data_dir' => '/abc')
     env = { 'workdir' => '/abc' }
 
