1401644ce122f999d0d7e1061154f8d168c5e949
Andrew Maraev <the_vk@thevk.net>
2018-10-15 08:37:24 -0700

    TaskShell allows to set process env variables

--- lib/nanoci/tasks/task_shell.rb
+++ lib/nanoci/tasks/task_shell.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require 'nanoci/common_vars'
+require 'nanoci/definition/variable_definition'
 require 'nanoci/task'
+require 'nanoci/tasks/task_shell_definition'
 require 'nanoci/tool_process'
+require 'nanoci/variable'
 
 class Nanoci
   class Tasks
@@ -19,7 +22,25 @@ class Nanoci
       end
 
       def execute_imp(build, env)
-        ToolProcess.run(@definition.cmd, chdir: env[CommonVars::WORKDIR], stdout: build.output, stderr: build.output)
+        task_env_vars = variables.map { |x| [x.tag, x.expand(env)] }
+                                 .to_h
+                                 .transform_keys { |x| strip_var_prefix(x) }
+
+        ToolProcess.run(@definition.cmd,
+                        env: task_env_vars,
+                        chdir: env[CommonVars::WORKDIR],
+                        stdout: build.output,
+                        stderr: build.output).wait
+      end
+
+      private
+
+      def variables
+        definition.env.map { |x| Variable.new(x) }
+      end
+
+      def strip_var_prefix(name)
+        name.to_s.sub("#{TaskShellDefinition::VARIABLE_PREFIX}.", '')
       end
     end
   end
--- lib/nanoci/tasks/task_shell_definition.rb
+++ lib/nanoci/tasks/task_shell_definition.rb
@@ -1,18 +1,41 @@
 # frozen_string_literal: true
 
 require 'nanoci/definition/task_definition'
+require 'nanoci/definition/variable_definition'
 
 class Nanoci
   class Tasks
+    # Definition for [TaskShell]
     class TaskShellDefinition < Nanoci::Definition::TaskDefinition
+
+      # Prefix added to variable tag
+      VARIABLE_PREFIX = "task_shell"
+
       # Returns the command to execute
       # @return [String]
       attr_reader :cmd
+
+      # Returns a set of environment variables
+      # @return [Hash<Symbol, VariableDefinition>]
+      attr_reader :env
 
       def initialize(hash)
         super(hash)
 
         @cmd = hash.fetch(:cmd, '')
+        @env = read_env(hash.fetch(:env, []))
+      end
+
+      private
+
+      # Reads a set of env variables from source
+      # @param env_vars [Array<Hash>]
+      # @return [Hash<Symbol, VariableDefinition>]
+      def read_env(env_vars)
+        env_vars.map do |x|
+          x = x.transform_keys { |k| "#{VARIABLE_PREFIX}.#{k}".to_sym }
+          Nanoci::Definition::VariableDefinition.new(x)
+        end
       end
     end
   end
