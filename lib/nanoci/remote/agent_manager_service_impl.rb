c8b0f52461b59c4f469a6266e2e704ad825a5661
Andrew Maraev <the_vk@thevk.net>
2018-12-12 22:00:00 -0800

    renamed rpc service to AgentManager

---
+++ lib/nanoci/remote/agent_manager_service_impl.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'nanoci_remote/agent_manager_services_pb'
+
+class Nanoci
+  class Remote
+    class AgentManagerServiceImpl < NanociRemote::AgentManager::Service
+      def report_agent_status()
+    end
+  end
+end
