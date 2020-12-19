f3ba0c402af904246a3f29ed00ca8a3f64f5293a
Andrew Maraev <the_vk@thevk.net>
2020-12-18 20:46:29 -0500

    remove usage of obsolete JobDefinition from Job
    Code DSL for project definition #1

--- lib/nanoci/job.rb
+++ lib/nanoci/job.rb
@@ -1,32 +1,19 @@
 # frozen_string_literal: true
 
-require 'nanoci/definition/job_definition'
-require 'nanoci/task'
-require 'nanoci/tasks/all'
-
 module Nanoci
   # A job is a collection of tasks to run actions and produce artifacts
   class Job
+    # @return [Symbol]
     attr_accessor :tag
-    attr_accessor :tasks
+
+    # @return [Proc]
+    attr_accessor :body
 
     # Initializes new instance of [Job]
-    # @param definition [JobDefinition]
-    def initialize(definition)
-      @tag = definition.tag
-      @tasks = read_tasks(definition.tasks)
-    end
-
-    # Returns required agent capabilities to run the job.
-    # @return [Set<Symbol>]
-    def required_agent_capabilities(build)
-      @tasks.map { |t| t.required_agent_capabilities(build) }.reduce(:merge)
-    end
-
-    private
-
-    def read_tasks(task_definitions)
-      task_definitions.map { |d| Task.resolve(d.type).new(d) }
+    # @param src [Hash]
+    def initialize(src)
+      @tag = src[:tag]
+      @body = src[:block]
     end
   end
 end
