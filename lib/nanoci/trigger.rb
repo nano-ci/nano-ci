eff68bd3838fe738752347daca5f59464d27ae8a
Andrew Maraev <the_vk@thevk.net>
2020-12-18 20:23:33 -0500

    removing obsolete TriggerDefinition class
    Code DSL for project definition #1

--- lib/nanoci/trigger.rb
+++ lib/nanoci/trigger.rb
@@ -14,42 +14,25 @@ module Nanoci
       'trigger'
     end
 
+    # @return [Symbol]
+    attr_reader :tag
+
+    # @return [Symbol]
     attr_reader :type
-    attr_reader :repo
 
     # Initializes new instance of [Trigger]
-    # @param repo [Repository]
-    # @param definition [TriggerDefinition]
-    def initialize(repo, definition)
+    # @param definition [Hash]
+    def initialize(hash)
       @log = Logging.logger[self]
-      @repo = repo
-      @type = definition.type
+      @tag = hash[:tag]
+      @type = hash[:type]
     end
 
     def run(build_scheduler, project)
       @build_scheduler = build_scheduler
       @project = project
 
-      @log.info("running trigger #{repo.tag}.#{type}")
-    end
-
-    def repo_has_changes?(repo)
-      repo.changes?
-    rescue StandardError => e
-      @log.error "failed to check repo #{repo.tag} for new changes"
-      @log.error e
-      false
-    end
-
-    def trigger_build
-      @log.info "checking repo #{@repo.tag} for new changes"
-      if repo_has_changes?(@repo)
-        @log.info "detected new changes in repo #{@repo.tag}" \
-          ', triggering a new build'
-        @build_scheduler.trigger_build(@project, self)
-      else
-        @log.info "repo #{@repo.tag} has no new changes"
-      end
+      @log.info("running trigger #{tag}")
     end
   end
 end
