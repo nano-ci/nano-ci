10d3f65757ee7c370120bf05ac631616d96cd1c5
Andrew Maraev <the_vk@thevk.net>
2020-12-22 20:29:25 -0500

    add #state to Job and module Job::State
    Pipeline engine #9

--- lib/nanoci/job.rb
+++ lib/nanoci/job.rb
@@ -4,16 +4,25 @@ module Nanoci
   # A job is a collection of tasks to run actions and produce artifacts
   class Job
     # @return [Symbol]
-    attr_accessor :tag
+    attr_reader :tag
 
     # @return [Proc]
-    attr_accessor :body
+    attr_reader :body
+
+    attr_reader :state
+
+    def state=(next_state)
+      raise "invalid state #{next_state}" unless State::VALUES.include? next_state
+
+      @state = next_state
+    end
 
     # Initializes new instance of [Job]
     # @param src [Hash]
     def initialize(src)
       @tag = src[:tag]
       @body = src[:block]
+      @state = State::IDLE
     end
   end
 end
