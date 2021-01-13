1e9f268f4ea2c8a84c85c5d4ccf1f3c73d9c1a9a
Andrew Maraev <the_vk@thevk.net>
2021-01-12 20:58:19 -0500

    minor refactoring of command_host.rb and pipeline_engine.rb

--- lib/nanoci/command_host.rb
+++ lib/nanoci/command_host.rb
@@ -8,8 +8,22 @@ module Nanoci
   # [CommandHost] is a class that executes Job's commands.
   class CommandHost
     # Runs Job's block with given inputs
-    def run(inputs, &block)
-      instance_exec(inputs, &block)
+    # @param inputs [Hash]
+    # @param prev_inputs [Hash]
+    # @yield [inputs, prev_inputs] Executes job body
+    # @yieldparam inputs [Hash]
+    # @yieldparam prev_inputs [Hash]
+    def run(inputs, prev_inputs, &block)
+      case block.arity
+      when 0
+        instance_exec(&block)
+      when 1
+        instance_exec(inputs, &block)
+      when 2
+        instance_exec(inputs, prev_inputs, &block)
+      else
+        raise ArgumentError, "job body block has invalid number of arguments (got #{block.arity}, expected 0..2"
+      end
     end
 
     # Executes passed command line
