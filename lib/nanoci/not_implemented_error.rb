e004cdf4632fe859d26ee6d2e67f526df794d77b
Andrew Maraev <the_vk@thevk.net>
2021-01-18 19:29:31 -0500

    fix rubocop issue in not_implemented_error.rb

--- lib/nanoci/not_implemented_error.rb
+++ lib/nanoci/not_implemented_error.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Nanoci
+  # Error that's thrown when a requested method is not implemented.
   class NotImplementedError < StandardError
     def initialize(class_name, method_name)
       super("#{class_name}::#{method_name} is not implemented")
