03927384a7fd379d6706fb33a9dd778814de3e8c
Andrew Maraev <the_vk@thevk.net>
2021-09-19 11:49:23 -0700

    add basic spec for Trigger#full_tag

--- spec/unit/trigger_spec.rb
+++ spec/unit/trigger_spec.rb
@@ -5,5 +5,8 @@ require 'spec_helper'
 require 'nanoci/trigger'
 
 RSpec.describe Nanoci::Trigger do
-
+  it '#full_tag returns "trigger.tag"' do
+    trigger = Nanoci::Trigger.new(tag: :trigger_tag, type: :test)
+    expect(trigger.full_tag).to eq :'trigger.trigger_tag'
+  end
 end
