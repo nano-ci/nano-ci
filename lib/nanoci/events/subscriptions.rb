e4d18f10805f643c020628797a599fbfd541413c
Andrew Maraev <the_vk@thevk.net>
2021-11-27 21:48:46 -0800

    add modules Topics and Subscriptions
    the modules contains topics and subscriptions names
    
    Workflow message subscribers #25

---
+++ lib/nanoci/events/subscriptions.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Nanoci
+  module Events
+    module Subscriptions
+      EXECUTE_JOB = :execute_job
+    end
+  end
+end
---
+++ lib/nanoci/events/topics.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Nanoci
+  module Events
+    module Topics
+      EXECUTE_JOB = :execute_job
+      JOB_FINISHED = :job_finished
+    end
+  end
+end
