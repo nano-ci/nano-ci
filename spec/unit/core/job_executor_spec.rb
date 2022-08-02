9249cfd637fdb428aeb4f37930545b97eb40ac17
Andrew Maraev <the_vk@thevk.net>
2022-08-02 12:36:16 -0700

    add specs for JobExecutor
    Split nano-ci into core module with business logic and app module #29

---
+++ spec/unit/core/job_executor_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+require 'nanoci/core/job_executor'
+
+# JobExecutor implementation for tests
+class TestJobExecutor < Nanoci::Core::JobExecutor
+  def raise_publish(stage, job, outputs)
+    publish(stage, job, outputs)
+  end
+end
+
+RSpec.describe Nanoci::Core::JobExecutor do
+  it '#publish raises event job_complete' do
+    executor = TestJobExecutor.new(nil)
+
+    sender = nil
+    event_args = nil
+
+    executor.job_complete.attach do |s, e|
+      sender = s
+      event_args = e
+    end
+
+    executor.raise_publish(:stage, :job, :outputs)
+
+    expect(sender).to be executor
+    expect(event_args).to be_a(Nanoci::Core::JobCompleteEventArgs)
+    expect(event_args.stage).to eq(:stage)
+    expect(event_args.job).to eq(:job)
+    expect(event_args.outputs).to eq(:outputs)
+  end
+end
