4d7fd42df7b423e1e872b8b2e6765c7e6bd9aaf6
Andrew Maraev <the_vk@thevk.net>
2022-08-04 12:24:02 -0700

    delete obsolete code in Repo
    Split nano-ci into core module with business logic and app module #29

--- lib/nanoci/core/repo.rb
+++ lib/nanoci/core/repo.rb
@@ -28,10 +28,6 @@ module Nanoci
       # Object specifies authentication against repo
       attr_reader :auth
 
-      # Collection of capabilities requred to run
-      # a job against the repo on an agent
-      attr_reader :required_agent_capabilities
-
       # Initializes new instance of Repo
       def initialize(tag:, type:, uri:, auth: nil)
         @log = Logging.logger[self]
@@ -40,46 +36,6 @@ module Nanoci
         @type = type.to_sym
         @uri = uri
         @auth = auth
-        @required_agent_capabilities = []
-        @current_commit = ''
-      end
-
-      def repo_cache
-        repo_path = File.join(Config::UCS.instance.repo_cache, tag.to_s)
-        FileUtils.mkdir_p(repo_path)
-        repo_path
-      end
-
-      ##
-      # Detect changes in source
-      # Returns true is there are new changes; false otherwise
-      def changes?
-        true
-      end
-
-      def tip_of_tree(_workdir, _branch)
-        ''
-      end
-
-      def clone(workdir, opts = {}); end
-
-      def exists?(workdir, opts = {}); end
-
-      def checkout(workdir, branch, opts = {}); end
-
-      def state
-        {
-          tag: tag,
-          current_commit: @current_commit
-        }
-      end
-
-      def state=(value)
-        @log.debug("restoring state of repo #{tag} from #{value}")
-        raise "tag #{tag} does not match state tag #{value[:tag]}" \
-          unless tag == value[:tag].to_sym
-
-        @current_commit = value[:current_commit]
       end
     end
   end
