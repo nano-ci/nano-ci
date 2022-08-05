0523f8cefa17766cf216f461be144be00769b6d4
Andrew Maraev <the_vk@thevk.net>
2022-08-04 22:03:26 -0700

    add extra test for StageDSL
    Split nano-ci into core module with business logic and app module #29

--- spec/unit/dsl/stage_dsl_spec.rb
+++ spec/unit/dsl/stage_dsl_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe Nanoci::DSL::StageDSL do
     stage = dsl.build
     expect(stage.triggering_inputs).to eq [:abc]
   end
+
+  it '#job adds new JobDSL to output' do
+    dsl = Nanoci::DSL::StageDSL.new(nil, :stage, inputs: [:abc])
+    # rubocop:disable Lint/EmptyBlock
+    dsl.job(:tag) do
+    end
+    # rubocop:enable Lint/EmptyBlock
+
+    stage = dsl.build
+    expect(stage.jobs.empty?).to be false
+  end
 end
