b195e046b7348ab9b2f71da0abcd5b861440a48b
Andrew Maraev <the_vk@thevk.net>
2022-09-21 22:02:57 -0700

    refactor TriggerRepository
    split business logic and storage methods
    
    issue #44

--- lib/nanoci/trigger_repository.rb
+++ lib/nanoci/trigger_repository.rb
@@ -22,32 +22,27 @@ module Nanoci
 
     def add(trigger)
       trigger_memento = trigger.memento
-      stored_trigger = @triggers.find { |t| t[:tag] == trigger_memento[:tag] }
-      if stored_trigger.nil?
-        enchance_initial_memento(trigger_memento)
-        @triggers.push trigger_memento
+      doc = find_by_tag(trigger_memento[:tag])
+      if doc.nil?
+        trigger_memento[FIELD_LOCK] = LOCK_WAITING
+        trigger_memento[FIELD_LOCK_EXPIRES] = nil
+        insert_doc(memento_to_doc(trigger_memento))
       else
-        trigger_memento = stored_trigger
+        trigger_memento = doc_to_memento(doc)
       end
       trigger.memento = trigger_memento
     end
 
     def read_and_lock_next_due_trigger(now_timestamp)
-      trigger_memento = @triggers.find { |t| t[:next_run_time] < now_timestamp && t[FIELD_LOCK] == LOCK_WAITING }
-      return nil if trigger_memento.nil?
+      doc = find_and_lock_due_doc(now_timestamp, LOCK_WAITING)
+      return if doc.nil?
 
-      trigger_memento[FIELD_LOCK] = LOCK_EXECUTING
-      trigger_memento[FIELD_LOCK_EXPIRES] = Time.now.utc + LOCK_TIMEOUT
-
-      hydrate_trigger(trigger_memento)
+      hydrate_trigger(doc_to_memento(doc))
     end
 
     def update_and_release_trigger(trigger)
       memento = trigger.memento
-      stored_memento = @triggers.find { |t| t[:id] = memento[:id] && t[FIELD_LOCK] == LOCK_EXECUTING }
-      stored_memento.merge!(memento)
-      stored_memento[FIELD_LOCK] = LOCK_WAITING
-      stored_memento[FIELD_LOCK_EXPIRES] = nil
+      update_and_release_doc(memento[:id], memento_to_doc(memento))
     end
 
     def due_triggers?(now_timestamp)
@@ -56,11 +51,44 @@ module Nanoci
 
     protected
 
-    def enchance_initial_memento(memento)
-      memento[:id] = @counter
+    def memento_to_doc(memento)
+      doc = memento.clone
+      doc[:_id] = doc[:id]
+      doc.delete :id
+      doc
+    end
+
+    def doc_to_memento(doc)
+      memento = doc.clone
+      memento[:id] = memento[:_id]
+      memento.delete :_id
+      memento
+    end
+
+    def find_by_tag(tag)
+      @triggers.find { |t| t[:tag] == tag }
+    end
+
+    def find_and_lock_due_doc(now_timestamp, state)
+      doc = @triggers.find { |t| t[:next_run_time] < now_timestamp && t[FIELD_LOCK] == state }
+      return nil if doc.nil?
+
+      doc[FIELD_LOCK] = LOCK_EXECUTING
+      doc[FIELD_LOCK_EXPIRES] = Time.now.utc + LOCK_TIMEOUT
+      doc
+    end
+
+    def update_and_release_doc(id, doc)
+      stored_memento = @triggers.find { |t| t[:_id] = id && t[FIELD_LOCK] == LOCK_EXECUTING }
+      stored_memento.merge!(doc)
+      stored_memento[FIELD_LOCK] = LOCK_WAITING
+      stored_memento[FIELD_LOCK_EXPIRES] = nil
+    end
+
+    def insert_doc(doc)
+      doc[:_id] = @counter
       @counter += 1
-      memento[FIELD_LOCK] = LOCK_WAITING
-      memento[FIELD_LOCK_EXPIRES] = nil
+      @triggers.push doc
     end
 
     def hydrate_trigger(memento)
