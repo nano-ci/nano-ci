b1c961e57a8fd0c1e8eb441606b3de1dbcca52a0
Andrew Maraev <the_vk@thevk.net>
2023-02-07 19:37:04 -0800

    fix failing rspecs

--- spec/unit/core/job_executor_spec.rb
+++ spec/unit/core/job_executor_spec.rb
@@ -13,20 +13,13 @@ end
 
 RSpec.describe Nanoci::Core::JobExecutor do
   it '#schedule_job_execution raises error NotImplementedError' do
-    executor = Nanoci::Core::JobExecutor.new(nil)
+    executor = Nanoci::Core::JobExecutor.new(nil, nil)
     expect { executor.schedule_job_execution(nil, nil, nil, nil, nil) }.to raise_error RuntimeError
   end
 
   it '#publish raises event job_complete' do
-    executor = TestJobExecutor.new(nil)
-
-    sender = nil
-    event_args = nil
-
-    executor.job_complete.attach do |s, e|
-      sender = s
-      event_args = e
-    end
+    topic = double(:topic)
+    executor = TestJobExecutor.new(nil, topic)
 
     project = double(:project)
     allow(project).to receive(:tag).and_return(:project)
@@ -37,13 +30,14 @@ RSpec.describe Nanoci::Core::JobExecutor do
     job = double(:job)
     allow(job).to receive(:tag).and_return(:job)
 
-    executor.raise_publish(project, stage, job, :outputs)
+    expect(topic).to receive(:publish) do |m|
+      expect(m).to be_a(Nanoci::Core::Messages::JobCompleteMessage)
+      expect(m.project_tag).to eq(:project)
+      expect(m.stage_tag).to eq(:stage)
+      expect(m.job_tag).to eq(:job)
+      expect(m.outputs).to eq(:outputs)
+    end
 
-    expect(sender).to be executor
-    expect(event_args).to be_a(Nanoci::Core::JobCompleteEventArgs)
-    expect(event_args.project_tag).to eq(:project)
-    expect(event_args.stage_tag).to eq(:stage)
-    expect(event_args.job_tag).to eq(:job)
-    expect(event_args.outputs).to eq(:outputs)
+    executor.raise_publish(project, stage, job, :outputs)
   end
 end
